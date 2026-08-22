844171c229a036ae03a862a0fa952393b519bd32
ewgsta <hello@ewgsta.me>
2026-08-22 14:13:21 +0300

    ... .. --.. .. / .. --.. .-.. .. -.-- --- .-. ..- -

--- src/pages/(home)/_root.tsx
+++ src/pages/(home)/_root.tsx
@@ -5,7 +5,13 @@ import '@/styles/globals.css';
 export default async function RootElement({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head></head>
+      <head>
+        <script
+          defer
+          src="https://analytics.ewgsta.me/script.js"
+          data-website-id="0f8168e0-f5b3-429c-a987-fe7c2fc566ca"
+        />
+      </head>
       <body data-version="1.0" className="flex flex-col min-h-screen">
         <Provider>{children}</Provider>
       </body>
